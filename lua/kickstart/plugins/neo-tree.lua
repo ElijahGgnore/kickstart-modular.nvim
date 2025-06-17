5dc002f38c0a48ba2b27757915d02e51d211d742
ElijahGgnore <elijahg.gnore@gmail.com>
2025-06-17 14:50:21 +1000

    auto close neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -4,6 +4,15 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
+  init = function()
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      desc = 'Close neo tree before leaving to avoid session loading conflict',
+      group = vim.api.nvim_create_augroup('NeoTree_custom', { clear = true }),
+      callback = function()
+        require('neo-tree.command').execute { action = 'close' }
+      end,
+    })
+  end,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -19,6 +28,18 @@ return {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+    event_handlers = {
+
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
+          require('neo-tree.command').execute { action = 'close' }
+        end,
       },
     },
   },
