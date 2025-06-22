44a77c8ce0a954f5f5318ab2f3ef15402bfc7d2b
ElijahGgnore <elijahg.gnore@gmail.com>
2025-06-22 13:09:11 +1000

    remove toggleterm taskwarrior tui

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,17 +1,6 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-    local taskwarrior = Terminal:new {
-      cmd = 'taskwarrior-tui',
-      display_name = 'Taskwarrior',
-      direction = 'float',
-      id = 3,
-    }
-
-    function _Taskwarrior_toggle()
-      taskwarrior:toggle()
-    end
-    vim.api.nvim_set_keymap('n', '<leader>W', '<cmd>lua _Taskwarrior_toggle()<CR>', { noremap = true, silent = true, desc = 'Task[W]arrior-tui' })
   opts = {
     open_mapping = [[<c-\>]],
     direction = 'float',
