6d92304da185bb4e3b4a28653ede4d0774fe79af
ElijahGgnore <elijahg.gnore@gmail.com>
2026-01-11 12:41:12 +1000

    Use basedpyright instead of mypy

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -208,7 +208,7 @@ return {
       local servers = {
         clangd = {},
         -- gopls = {},
-        pylsp = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
