e31ebc610df005196b96884b5598d1a57ec879ce
ElijahGgnore <elijahg.gnore@gmail.com>
2026-01-14 23:52:29 +1000

    Revert "Don't require neo-tree"
    This reverts commit ea0017d0563c6722ec4597797bc7f690f7b4f05d and
    bf71a2e.

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,46 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  init = function()
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      desc = 'Close neo tree before leaving to avoid session loading conflict',
+      group = vim.api.nvim_create_augroup('NeoTree_custom', { clear = true }),
+      callback = function()
+        require('neo-tree.command').execute { action = 'close' }
+      end,
+    })
+  end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+    event_handlers = {
+
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
+  },
+}
