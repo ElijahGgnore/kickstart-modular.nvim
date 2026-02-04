e576243532804af6e318dc98f45047cd39c1b278
ElijahGgnore <elijahg.gnore@gmail.com>
2026-02-04 14:26:43 +1000

    feat(formatting): make format-on-save togglable

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -13,6 +13,26 @@ return {
         desc = '[F]ormat buffer',
       },
     },
+    init = function()
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+    end,
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
@@ -20,7 +40,11 @@ return {
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
+        if
+            disable_filetypes[vim.bo[bufnr].filetype]
+            or vim.g.disable_autoformat
+            or vim.b[bufnr].disable_autoformat
+        then
           return nil
         else
           return {
