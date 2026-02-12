8dc13a7ce676869aca5144b370cf60fa99b436c1
ElijahGgnore <elijahg.gnore@gmail.com>
2026-02-12 14:19:49 +1000

    fix: add missing parenthesis

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -10,6 +10,7 @@ return {
         markdown = { 'markdownlint' },
 
         htmldjango = { 'djlint' },
+      }
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
