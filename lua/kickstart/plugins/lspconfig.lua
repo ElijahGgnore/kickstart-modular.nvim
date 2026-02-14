758b06d51f352d7de6e89dfa1be8eb523aa1ee88
ElijahGgnore <elijahg.gnore@gmail.com>
2026-02-14 15:01:10 +1000

    feat(LSP): add frontend-related LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -211,6 +211,12 @@ return {
         taplo = {},  -- TOML
         -- gopls = {},
         basedpyright = {},
+
+        html = {},
+        cssls = {},
+        json = {},
+        denols = {}, -- javascript
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
